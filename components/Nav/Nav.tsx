9e77203bfecb5a64ba2d96ccce3b735b11f851bf
Dave O’Brien <me@daveobrien.co.uk>
2022-12-28 22:51:28 +0000

    feat: Add active link to Nav

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
+import classNames from 'classnames'
 
 type Links = {
   href: string
@@ -14,9 +16,11 @@ type NavProps = {
 }
 
 export default function Nav({ delay = 0, duration = 0, links, showNav }: NavProps) {
+  const router = useRouter()
+
   return (
     <ul
-      className={`text-neutral-500 flex flex-initial gap-half md:mt-two lg:mt-three xl:mt-four justify-center transition-opacity ${
+      className={`text-neutral-500 flex flex-initial md:mt-two lg:mt-three xl:mt-four justify-center transition-opacity ${
         showNav ? 'opacity-1' : 'opacity-0'
       }`}
       style={{
@@ -25,9 +29,17 @@ export default function Nav({ delay = 0, duration = 0, links, showNav }: NavProp
       }}
     >
       {links.map(({ href, text }) => (
-        <li key={text} className="font-display font-black text-lg md:text-md lg:text-lg tracking-tighter uppercase">
-          {/* TODO: [P2] Add active link */}
-          <Link href={href} className="block hover:underline underline-offset-4 hover:text-neutral-700">
+        <li key={text}>
+          <Link
+            href={href}
+            className={classNames(
+              'block text-lg md:text-md lg:text-lg heading underline-offset-4 focus:outline-none px-quarter hover:underline focus:underline',
+              { 'text-primary-700 hover:text-primary-900 focus:text-primary-900': router.pathname === href },
+              {
+                'focus:text-neutral-700 hover:text-neutral-700': router.pathname !== href,
+              }
+            )}
+          >
             {text}
           </Link>
         </li>
