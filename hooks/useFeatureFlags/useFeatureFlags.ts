b7f14a766862223aea747ed1c997067e98ea40e6
Dave O’Brien <me@daveobrien.co.uk>
2023-01-11 15:51:54 +0000

    fix: Temp hardcode flags
    Flagsmith not working on second render (app getInitialState looks OK)

--- hooks/useFeatureFlags/useFeatureFlags.ts
+++ hooks/useFeatureFlags/useFeatureFlags.ts
@@ -1,17 +1,23 @@
+import { useEffect } from 'react'
 import { useFlags } from 'flagsmith/react'
 import type { FlagOptions, Link } from 'constants/'
 
 export default function useFeatureFlags() {
   const flags = useFlags<FlagOptions>(['section_about', 'section_cv', 'section_notes', 'section_project'])
 
-  const linksByFeatureFlag = (link: Link) => {
-    const linkFeatureFlag = link.featureFlag && flags[link.featureFlag]
+  // INFO: Hardcoded feature flags for now - flagsmith not working on second render (app getInitialState looks OK)
+  // TODO: #37 Fix flagsmith flags not working on second render (app getInitialState looks OK)
+  const linksByFeatureFlag = (link: Link) =>
+    !link.featureFlag || ['section_about', 'section_cv'].includes(link.featureFlag)
 
-    // If no flag exists, then return the link
-    if (!linkFeatureFlag) return true
+  // const linksByFeatureFlag = (link: Link) => {
+  //   const linkFeatureFlag = link.featureFlag && flags[link.featureFlag]
 
-    return linkFeatureFlag.enabled
-  }
+  //   // If no flag exists, then return the link
+  //   if (!linkFeatureFlag) return true
+
+  //   return linkFeatureFlag.enabled
+  // }
 
   const checkSomeFeatureFlags = (flagsToCheck: FlagOptions[]) => flagsToCheck.some((flag) => flags[flag].enabled)
 
